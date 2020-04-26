efa24eb3534a555d91a31af44893d5ad7298f0e7
goodGid <gy8971@gmail.com>
2020-04-26 10:25:56 +0900

    Set TimeZone

--- src/main/java/goodgid/odot/OdotApplication.java
+++ src/main/java/goodgid/odot/OdotApplication.java
@@ -1,10 +1,19 @@
 package goodgid.odot;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OdotApplication {
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(OdotApplication.class, args);
